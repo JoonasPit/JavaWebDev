30b628651f491beb00e3f7e9321203ef422d9799
JoonasPit <43230082+JoonasPit@users.noreply.github.com>
2019-04-02 11:23:17 +0300

    Update JoonasServlet.java

--- JoonasServlet.java
+++ JoonasServlet.java
@@ -1,36 +1,34 @@
-package helloWorld.control;
-
+package web1projekti.control;
+import javax.servlet.RequestDispatcher;
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class JoonasServlet
- */
-@WebServlet("/Joonas")
+@WebServlet("/joonas")
 public class JoonasServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+ 
     public JoonasServlet() {
         super();
+        // TODO Auto-generated constructor stub
     }
 
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("<h1>Tähän tulee ehkä pelin otsikko</h1>");
-		response.getWriter().append("Tämä on placeholder tekstiä</br>");
-		response.getWriter().append("Lisää tekstiä</br>");
-		response.getWriter().append("Tekstiä");
+		String jsp = "/view/projektietusivu.jsp";
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(jsp);
+		dispatcher.forward(request, response);
 		
 		
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
